d00760cb1be4b35b6601a339bca5b62a64920970
mikareich <mikareich0@gmail.com>
2020-02-02 14:34:58 +0100

    einiges

--- script/app.js
+++ script/app.js
@@ -1,45 +1,50 @@
-Array.prototype.update = function() {
+import ContextMenu from "./contextMenu.js";
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.update = function upload() {
   window.localStorage.setItem("notes", JSON.stringify(this));
   return this;
 };
 
-import ContextMenu from "./contextMenu.js";
-
-const searchNoteINPUT = document.querySelector("#searchNote");
+// const searchNoteINPUT = document.querySelector('#searchNote');
 const allNotes = document.querySelector("#notes");
 const notes = JSON.parse(window.localStorage.getItem("notes")) || [];
-const newNoteBTN = document.querySelector("#newNoteBTN");
-const newNoteINPUT = document.querySelector("#newNote");
+// const newNoteBTN = document.querySelector('#newNoteBTN');
+// const newNoteINPUT = document.querySelector('#newNote');
 const contextMenu = new ContextMenu(notes);
-if (notes.length > 0) notes.forEach(note => newNote(note));
 // save notes in localstorage
-newNoteBTN.addEventListener("click", function() {
+/* newNoteBTN.addEventListener('click', () => {
   // save note in array
-  if (newNoteINPUT.value.trim() === "") return;
+  if (newNoteINPUT.value.trim() === '') return;
   notes.push({
     content: newNoteINPUT.value.trim(),
     id: notes.length,
     checked: false,
-    marked: false
+    marked: false,
   });
   notes.update();
-  newNoteINPUT.value = "";
+  newNoteINPUT.value = '';
   newNoteINPUT.focus();
   // display note
   newNote(notes[notes.length - 1]);
   // save note in localstorage
-});
+}); */
 
 function newNote(note) {
+  // create note children
   const newNoteLI = document.createElement("li");
   newNoteLI.id = note.id;
-  // create note children
+  newNoteLI.style.cssText = `--x:${note.id * 35}`
+  // create header
+  const headerDIV = document.createElement("div");
+  headerDIV.classList.add("header");
+  // create checkbox
   const checkboxIMG = document.createElement("img");
   checkboxIMG.src = note.checked
     ? "../assets/check_box-24px.svg"
     : "../assets/check_box_outline_blank-24px.svg";
   checkboxIMG.classList.add("checkbox");
-  checkboxIMG.addEventListener("click", function() {
+  checkboxIMG.addEventListener("click", () => {
     newNoteLI.classList.toggle("checked");
     note.checked = !note.checked;
     notes.update();
@@ -47,13 +52,14 @@ function newNote(note) {
       ? "../assets/check_box-24px.svg"
       : "../assets/check_box_outline_blank-24px.svg";
   });
-
+  checkboxIMG.alt = "checkbox";
   if (note.checked) newNoteLI.classList.add("checked");
   if (note.marked) newNoteLI.classList.add("marked");
+  // create contextMenu trigger
   const moreIMG = document.createElement("img");
   moreIMG.src = "./assets/more_vert-24px.svg";
   moreIMG.classList.add("more");
-  moreIMG.addEventListener("click", function() {
+  moreIMG.addEventListener("click", function updateContextMenu() {
     const { x, y } = this.getBoundingClientRect();
     contextMenu.x = x;
     contextMenu.y = y;
@@ -61,18 +67,30 @@ function newNote(note) {
     contextMenu.update(note);
     contextMenu.display(true);
   });
-  const span = document.createElement("span");
-  span.textContent = note.content;
+  moreIMG.alt = "more options";
+  // create title
+  const titleSpan = document.createElement("span");
+  titleSpan.classList.add("title");
+  titleSpan.textContent = note.title;
+  // add childrens to header
+  headerDIV.appendChild(titleSpan);
+  headerDIV.appendChild(checkboxIMG);
+  headerDIV.appendChild(moreIMG);
+
+  // add main container
+  const mainSpan = document.createElement("span");
+  mainSpan.classList.add("main");
+  mainSpan.textContent = note.content;
+
   // add children
-  newNoteLI.appendChild(span);
-  newNoteLI.appendChild(checkboxIMG);
-  newNoteLI.appendChild(moreIMG);
+  newNoteLI.appendChild(headerDIV);
+  newNoteLI.appendChild(mainSpan);
   // display note
   allNotes.appendChild(newNoteLI);
 }
-searchNoteINPUT.addEventListener("keyup", searchNote);
+if (notes.length > 0) notes.forEach(note => newNote(note));
 
-function searchNote() {
+/* function searchNote() {
   const content = searchNoteINPUT.value.trim();
   // search notes
   let identifiedElements = notes
@@ -89,6 +107,8 @@ function searchNote() {
     else li.hidden = false;
   });
 }
+searchNoteINPUT.addEventListener("keyup", searchNote);
+ */
 
 /* if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
